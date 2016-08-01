181d33b1f92f1811acf2d622c86d5ee73b9de28e
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-08-01 13:17:07 -0400

    moved race tests to race_test.go file

---
+++ race_test.go
@@ -0,0 +1,60 @@
+// +build race
+
+package congomap
+
+import (
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+func testRace(t *testing.T, cgm Congomap) {
+	const tasks = 1000
+	keys := []string{"just", "a", "few", "keys", "to", "force", "lock", "contention"}
+
+	var wg sync.WaitGroup
+	wg.Add(tasks)
+
+	for i := 0; i < tasks; i++ {
+		go func(cgm Congomap, wg *sync.WaitGroup, keys []string) {
+			const iterations = 1000
+
+			for j := 0; j < iterations; j++ {
+				key := keys[rand.Intn(len(keys))]
+				if j%4 == 0 {
+					cgm.Delete(key)
+				} else {
+					_, _ = cgm.LoadStore(key)
+				}
+			}
+			wg.Done()
+		}(cgm, &wg, keys)
+	}
+
+	wg.Wait()
+}
+
+func TestRaceChannelMap(t *testing.T) {
+	cgm, _ := NewChannelMap(Lookup(randomFailOnLookup), TTL(time.Second))
+	testRace(t, cgm)
+	defer cgm.Close()
+}
+
+func TestRaceSyncAtomicMap(t *testing.T) {
+	cgm, _ := NewSyncAtomicMap(Lookup(randomFailOnLookup), TTL(time.Second))
+	testRace(t, cgm)
+	defer cgm.Close()
+}
+
+func TestRaceSyncMutexMap(t *testing.T) {
+	cgm, _ := NewSyncMutexMap(Lookup(randomFailOnLookup), TTL(time.Second))
+	testRace(t, cgm)
+	defer cgm.Close()
+}
+
+func TestRaceTwoLevelMap(t *testing.T) {
+	cgm, _ := NewTwoLevelMap(Lookup(randomFailOnLookup), TTL(time.Second))
+	testRace(t, cgm)
+	defer cgm.Close()
+}
